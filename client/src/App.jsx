5fe4d28c338349cbc3e2b2e47e41f62621a431e2
Sungjae.Kwak <sungjae8970@gmail.com>
2022-08-11 20:02:22 +0900

    ✨ react-router-dom도입

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,38 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Auth from './Components/Auth';
-import Todo from './Components/Todo';
+import Main from './Components/Main';
 
 function App() {
   const token = localStorage.getItem('token');
-  // console.log(token);
-  if (token) {
-    return (
-      <div style={{ border: '1px solid black' }} className="App">
-        <h1 style={{color: 'tomato'}}>App.jsx</h1>
-        <Todo />
-      </div>
-    );
-  }
+
   return (
-    <div className='App'>
-      <h1 style={{color: 'tomato'}}>App.jsx</h1>
-      <Auth />
-    </div>
+    <BrowserRouter>
+      <Link to='/auth'>Auth</Link>
+      <br />
+      <Link to='/'>Main</Link>
+      <Routes>
+        <Route path={"/"} element={<Main token={token} />} />
+        <Route path={"/:id"} element={<Main token={token} />} />
+        <Route path={"/update/:id"} element={<Main token={token} />} />
+        <Route path={"/auth"} element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   )
+
+  // if (token) {
+  //   return (
+  //     <div style={{ border: '1px solid black' }} className="App">
+  //       <h1 style={{color: 'tomato'}}>App.jsx</h1>
+  //       <Main token={token} />
+  //     </div>
+  //   );
+  // }
+  // return (
+  //   <div className='App'>
+  //     <h1 style={{color: 'tomato'}}>App.jsx</h1>
+  //     <Auth />
+  //   </div>
+  // )
 }
 
 export default App;
